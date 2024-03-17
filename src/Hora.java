3249786d7fd986c9bb871417768886a52ae5fb7e
AngelsGV <angels.not99@gmail.com>
2024-03-17 16:27:14 +0100

    Initial commit

--- src/Hora.java
+++ src/Hora.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Hora {
     protected int hora,minutos; //atributos protegidos, pensados para heredar.
     Hora (int hora, int minutos){
@@ -44,5 +46,22 @@ public class Hora {
         String result;
         result = hora + ":" + minutos;
         return result;
+    }
+
+    static public void main(String args[]) {
+        Hora r = new Hora(11, 30);
+        System.out.println(r);
+        for (int i=1;i <= 61;i++){
+            r.inc();
+        }
+        System.out.println(r);
+        System.out.println("Escriba una hora: ");
+        int hora = new Scanner(System.in).nextInt();
+        boolean cambio = r.setHora(hora);
+        if (cambio) {
+            System.out.println(r);
+        } else {
+            System.out.println("La hora no se pudo cambiar.");
+        }
     }
 }
